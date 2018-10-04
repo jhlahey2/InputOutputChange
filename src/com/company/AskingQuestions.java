417c8df9b792a51a791ebc5f2cc5dd25e1f01fa1
jhlahey2 <jhlahey@protonmail.com>
2018-10-04 09:43:41 -0400

    Initial commit

---
+++ src/com/company/AskingQuestions.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**\
+ * Assignment
+ * Input/Output Change Assignment
+ * This application takes 2 integer values and finds the largest one.
+ * Change the application so that it finds the average of two double values.
+ *
+ */
+
+public class AskingQuestions
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+//        int num1, num2, num3;
+        double num1, num2;
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextInt();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextInt();
+
+//        System.out.println("The max value is : " + Math.max(num1, num2));
+        System.out.println("The average value is " + ((num1 + num2) / 2));
+    }
+}
